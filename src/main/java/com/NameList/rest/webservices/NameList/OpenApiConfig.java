5e535e2ed11fd17adc9c2187d10fc43d072f425c
Shivanshu Dwivedi <143759395+shivanshudwivedi@users.noreply.github.com>
2024-02-20 14:01:09 -0500

    Create OpenApiConfig.java
    Swagger Configuration

---
+++ src/main/java/com/NameList/rest/webservices/NameList/OpenApiConfig.java
@@ -0,0 +1,17 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI usersMicroserviceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("NameList")
+                                 .description("Helps you add names to a List")
+                                 .version("1.0"));
+    }
+}
